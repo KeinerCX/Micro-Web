a352da54fc3e83cb12c411646d789024ce6bad16
Ashton Quinn <ash@pop-os.localdomain>
2022-05-02 16:23:15 -0500

    added ui error handling

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,34 +1,41 @@
 import { createTRPCClient } from "@trpc/client";
-import React, { FormEventHandler } from "react";
-import {usersServiceRouter} from "../../users/src/router"
+import React, { FormEventHandler, useState } from "react";
+import { usersServiceRouter } from "../../users/src/router";
 
 export default function Web() {
+  let [message, setMessage] = useState("");
+
   const registerUser = async (event: any) => {
-    event.preventDefault() 
+    event.preventDefault();
 
     let client = createTRPCClient<usersServiceRouter>({
-      url: "http://localhost:3001"
-    })
+      url: "http://localhost:3001",
+    });
 
     console.log({
       username: event.target.name.value!,
       password: event.target.password.value!,
       access_code: event.target.access_code.value!,
       email: event.target.email.value!,
-    })
+    });
 
-    let data = await client.mutation("register", {
-      username: event.target.name.value!,
-      password: event.target.password.value!,
-      access_code: event.target.access_code.value!,
-      email: event.target.email.value!,
-    })
+    try {
+      let data = await client.mutation("register", {
+        username: event.target.name.value!,
+        password: event.target.password.value!,
+        access_code: event.target.access_code.value!,
+        email: event.target.email.value!,
+      });
 
-  }
+      setMessage(`Successfully registered as ${event.target.name.value!}`);
+    } catch (e: any) {
+      setMessage(e.message);
+    }
+  };
   return (
     <div>
-      <h1>Web</h1>
-
+      <h1>Web App</h1>
+      <h3>{message}</h3>
       <form onSubmit={registerUser}>
         <label htmlFor="name">Username</label>
         <input id="name" type="text" autoComplete="username" required />
@@ -36,14 +43,17 @@ export default function Web() {
         <label htmlFor="name">Email</label>
         <input id="email" type="text" autoComplete="email" required />
 
-
         <label htmlFor="name">Password</label>
         <input id="password" type="password" autoComplete="password" required />
 
         <hr></hr>
         <label htmlFor="name">Access Code</label>
-        <input id="access_code" type="text" autoComplete="access_code" required />
-
+        <input
+          id="access_code"
+          type="text"
+          autoComplete="access_code"
+          required
+        />
 
         <button type="submit">Register</button>
       </form>
