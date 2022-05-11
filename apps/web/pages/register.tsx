d50b1fc1a6c92797205ebaf84deda3d400643fc9
TheModdedChicken <logan.h.shaw@gmail.com>
2022-05-11 21:29:19 +0800

    Man

--- apps/web/pages/register.tsx
+++ apps/web/pages/register.tsx
@@ -133,7 +133,6 @@ export default function Web() {
                   await Util.User.Login(email, password);
                   setMessage(`Successfully logged in as ${user.username}`)
 
-                  // Truely dumb
                   window.location.href = "/"
                 } catch (e: any) {
                   setMessage(e.message);
